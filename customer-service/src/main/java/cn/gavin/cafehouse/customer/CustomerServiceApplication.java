150af8ad68f4afd0d2984aeb5a14a595e17388d6
Gavin <13692238295@163.com>
2020-10-01 18:01:30 +0800

    consul配置剔除无效服务

--- customer-service/src/main/java/cn/gavin/cafehouse/customer/CustomerServiceApplication.java
+++ customer-service/src/main/java/cn/gavin/cafehouse/customer/CustomerServiceApplication.java
@@ -4,25 +4,24 @@ import cn.gavin.cafehouse.customer.support.CustomConnectionKeepAliveStrategy;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.client.RestTemplate;
 
-import java.time.Duration;
 import java.util.TimeZone;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
+@EnableAspectJAutoProxy
 public class CustomerServiceApplication {
     @Autowired
     private RestTemplate restTemplate;
@@ -32,37 +31,49 @@ public class CustomerServiceApplication {
     }
 
     @Bean
-    public HttpComponentsClientHttpRequestFactory requestFactory() {
-        PoolingHttpClientConnectionManager connectionManager =
-                new PoolingHttpClientConnectionManager(30, TimeUnit.SECONDS);
-        connectionManager.setMaxTotal(200);
-        connectionManager.setDefaultMaxPerRoute(20);
-
-        CloseableHttpClient httpClient = HttpClients.custom()
-                .setConnectionManager(connectionManager)
+    public CloseableHttpClient httpClient() {
+        return HttpClients.custom()
+                .setConnectionTimeToLive(30, TimeUnit.SECONDS)
                 .evictIdleConnections(30, TimeUnit.SECONDS)
+                .setMaxConnTotal(200)
+                .setMaxConnPerRoute(20)
                 .disableAutomaticRetries()
-                // 有 Keep-Alive 认里面的值，没有的话永久有效
-                //.setKeepAliveStrategy(DefaultConnectionKeepAliveStrategy.INSTANCE)
-                // 换成自定义的
                 .setKeepAliveStrategy(new CustomConnectionKeepAliveStrategy())
                 .build();
-
-        HttpComponentsClientHttpRequestFactory requestFactory =
-                new HttpComponentsClientHttpRequestFactory(httpClient);
-
-        return requestFactory;
     }
 
-    @LoadBalanced
-    @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder
-                .setConnectTimeout(Duration.ofMillis(1000))
-                .setReadTimeout(Duration.ofMillis(5000))
-                .requestFactory(this::requestFactory)
-                .build();
-    }
+//    @Bean
+//    public HttpComponentsClientHttpRequestFactory requestFactory() {
+//        PoolingHttpClientConnectionManager connectionManager =
+//                new PoolingHttpClientConnectionManager(30, TimeUnit.SECONDS);
+//        connectionManager.setMaxTotal(200);
+//        connectionManager.setDefaultMaxPerRoute(20);
+//
+//        CloseableHttpClient httpClient = HttpClients.custom()
+//                .setConnectionManager(connectionManager)
+//                .evictIdleConnections(30, TimeUnit.SECONDS)
+//                .disableAutomaticRetries()
+//                // 有 Keep-Alive 认里面的值，没有的话永久有效
+//                //.setKeepAliveStrategy(DefaultConnectionKeepAliveStrategy.INSTANCE)
+//                // 换成自定义的
+//                .setKeepAliveStrategy(new CustomConnectionKeepAliveStrategy())
+//                .build();
+//
+//        HttpComponentsClientHttpRequestFactory requestFactory =
+//                new HttpComponentsClientHttpRequestFactory(httpClient);
+//
+//        return requestFactory;
+//    }
+//
+//    @LoadBalanced
+//    @Bean
+//    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+//        return builder
+//                .setConnectTimeout(Duration.ofMillis(1000))
+//                .setReadTimeout(Duration.ofMillis(5000))
+//                .requestFactory(this::requestFactory)
+//                .build();
+//    }
 
     @Bean
     public Jackson2ObjectMapperBuilderCustomizer jacksonBuilderCustomizer() {
