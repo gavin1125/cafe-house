a0bfd4757ee25032e9eeb14f412dd71a9a0edd41
Gavin <13692238295@163.com>
2020-10-01 18:17:59 +0800

    consul配置剔除无效服务

--- customer-service/src/main/java/cn/gavin/cafehouse/customer/CustomerRunner.java
+++ customer-service/src/main/java/cn/gavin/cafehouse/customer/CustomerRunner.java
@@ -1,29 +1,17 @@
 package cn.gavin.cafehouse.customer;
 
-import cn.gavin.cafehouse.customer.model.Coffee;
-import cn.gavin.cafehouse.customer.model.CoffeeOrder;
-import cn.gavin.cafehouse.customer.model.NewOrderRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.RequestEntity;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Component
 @Slf4j
 public class CustomerRunner implements ApplicationRunner {
-    @Autowired
-    private RestTemplate restTemplate;
+//    @Autowired
+//    private RestTemplate restTemplate;
     @Autowired
     private DiscoveryClient discoveryClient;
 
@@ -42,32 +30,32 @@ public class CustomerRunner implements ApplicationRunner {
         });
     }
 
-    private void readMenu() {
-        ParameterizedTypeReference<List<Coffee>> ptr =
-                new ParameterizedTypeReference<List<Coffee>>() {};
-        ResponseEntity<List<Coffee>> list = restTemplate
-                .exchange("http://waiter-service/coffee/", HttpMethod.GET, null, ptr);
-        list.getBody().forEach(c -> log.info("Coffee: {}", c));
-    }
-
-    private Long orderCoffee() {
-        NewOrderRequest orderRequest = NewOrderRequest.builder()
-                .customer("Li Lei")
-                .items(Arrays.asList("capuccino"))
-                .build();
-        RequestEntity<NewOrderRequest> request = RequestEntity
-                .post(UriComponentsBuilder.fromUriString("http://waiter-service/order/").build().toUri())
-                .body(orderRequest);
-        ResponseEntity<CoffeeOrder> response = restTemplate.exchange(request, CoffeeOrder.class);
-        log.info("Order Request Status Code: {}", response.getStatusCode());
-        Long id = response.getBody().getId();
-        log.info("Order ID: {}", id);
-        return id;
-    }
-
-    private void queryOrder(Long id) {
-        CoffeeOrder order = restTemplate
-                .getForObject("http://waiter-service/order/{id}", CoffeeOrder.class, id);
-        log.info("Order: {}", order);
-    }
+//    private void readMenu() {
+//        ParameterizedTypeReference<List<Coffee>> ptr =
+//                new ParameterizedTypeReference<List<Coffee>>() {};
+//        ResponseEntity<List<Coffee>> list = restTemplate
+//                .exchange("http://waiter-service/coffee/", HttpMethod.GET, null, ptr);
+//        list.getBody().forEach(c -> log.info("Coffee: {}", c));
+//    }
+//
+//    private Long orderCoffee() {
+//        NewOrderRequest orderRequest = NewOrderRequest.builder()
+//                .customer("Li Lei")
+//                .items(Arrays.asList("capuccino"))
+//                .build();
+//        RequestEntity<NewOrderRequest> request = RequestEntity
+//                .post(UriComponentsBuilder.fromUriString("http://waiter-service/order/").build().toUri())
+//                .body(orderRequest);
+//        ResponseEntity<CoffeeOrder> response = restTemplate.exchange(request, CoffeeOrder.class);
+//        log.info("Order Request Status Code: {}", response.getStatusCode());
+//        Long id = response.getBody().getId();
+//        log.info("Order ID: {}", id);
+//        return id;
+//    }
+//
+//    private void queryOrder(Long id) {
+//        CoffeeOrder order = restTemplate
+//                .getForObject("http://waiter-service/order/{id}", CoffeeOrder.class, id);
+//        log.info("Order: {}", order);
+//    }
 }
