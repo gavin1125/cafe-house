f9fbafe00b5ee323e0be788373be1cca3b339a1e
Gavin <13692238295@163.com>
2020-10-02 08:09:08 +0800

    consul配置剔除无效服务

--- waiter-service/src/main/java/cn/gavin/cafehouse/waiter/model/CoffeeOrder.java
+++ waiter-service/src/main/java/cn/gavin/cafehouse/waiter/model/CoffeeOrder.java
@@ -1,6 +1,8 @@
 package cn.gavin.cafehouse.waiter.model;
 
 import lombok.*;
+import org.hibernate.annotations.Type;
+import org.joda.money.Money;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -9,6 +11,7 @@ import java.util.List;
 @Entity
 @Table(name = "T_ORDER")
 @Data
+@EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString(callSuper = true)
@@ -21,7 +24,13 @@ public class CoffeeOrder extends BaseEntity implements Serializable{
     private String customer;
     @ManyToMany
     @JoinTable(name = "T_ORDER_COFFEE")
+    @OrderBy("id")
     private List<Coffee> items;
     @Column(nullable = false)
     private OrderState state;
+    private Integer discount;
+    @Type(type = "org.jadira.usertype.moneyandcurrency.joda.PersistentMoneyAmount",
+            parameters = {@org.hibernate.annotations.Parameter(name = "currencyCode", value = "CNY")})
+    private Money total;
+    private String waiter;
 }
--- waiter-service/src/main/java/cn/gavin/cafehouse/waiter/service/CoffeeOrderService.java
+++ waiter-service/src/main/java/cn/gavin/cafehouse/waiter/service/CoffeeOrderService.java
@@ -4,15 +4,22 @@ import cn.gavin.cafehouse.waiter.model.Coffee;
 import cn.gavin.cafehouse.waiter.model.CoffeeOrder;
 import cn.gavin.cafehouse.waiter.model.OrderState;
 import cn.gavin.cafehouse.waiter.repository.CoffeeOrderRepository;
+import cn.gavin.cafehouse.waiter.support.OrderProperties;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
 import lombok.extern.slf4j.Slf4j;
+import org.joda.money.Money;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Slf4j
 @Service
@@ -20,11 +27,17 @@ import java.util.Arrays;
 public class CoffeeOrderService {
     @Autowired
     private CoffeeOrderRepository orderRepository;
+    @Autowired
+    private OrderProperties orderProperties;
+    private String waiterId = UUID.randomUUID().toString();
 
     public CoffeeOrder createOrder(String customer, Coffee...coffee) {
         CoffeeOrder order = CoffeeOrder.builder()
                 .customer(customer)
+                .discount(orderProperties.getDiscount())
+                .total(calcTotal(coffee))
                 .items(new ArrayList<>(Arrays.asList(coffee)))
+                .waiter(orderProperties.getWaiterPrefix() + waiterId)
                 .state(OrderState.INIT)
                 .build();
         CoffeeOrder saved = orderRepository.save(order);
@@ -45,5 +58,12 @@ public class CoffeeOrderService {
         orderRepository.save(order);
         log.info("Updated Order: {}", order);
         return true;
+    }
+
+    private Money calcTotal(Coffee...coffee) {
+        List<Money> items = Stream.of(coffee).map(c -> c.getPrice())
+                .collect(Collectors.toList());
+        return Money.total(items).multipliedBy(orderProperties.getDiscount())
+                .dividedBy(100, RoundingMode.HALF_UP);
     }
 }
